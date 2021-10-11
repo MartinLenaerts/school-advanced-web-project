2278fa6aeb9ebdaef3614fa63cd8238ba4d4473a
martin <martin.lenaerts@hotmail.fr>
2021-10-11 15:45:04 +0200

    modif App

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app);
